06529ec8d2e3bf5e010ae0687e81d29adc848ed0
sourav_378 <SouravSharma@policybazaar.com>
2022-10-03 23:20:58 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const age = 21
+const name = 'Naveen'
+
+console.log('age :', age)
+console.log('name :', name)
+
+//--------------------------------------
+
+const firstNum = 10
+const secondNum = 5
+
+const sum = firstNum + secondNum
+const multiply = firstNum * secondNum
+
+console.log('sum :', sum)
+console.log('multiply :', multiply)
+
+const person = {
+    name: "naveen",
+    age: 21,
+    occupation: 'enineer',
+    height: "5.7",
+    hobbies: ['cycling', 'bodyBuilding', 'eating']
+}
+
